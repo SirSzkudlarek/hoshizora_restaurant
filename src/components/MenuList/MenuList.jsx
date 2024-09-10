3b0b045aee7eb4c4cde968172637732e221620d6
Damian Szkudlarek <damian.szkudlarek@yuush.com>
2024-09-10 05:53:57 +0200

    Menu is full of content and optymalized for all diveces

--- src/components/MenuList/MenuList.jsx
+++ src/components/MenuList/MenuList.jsx
@@ -4,10 +4,11 @@ import { FaYenSign } from 'react-icons/fa6';
 import { images } from '../../constants';
 import './MenuList.css';
 
-const MenuItem = ({ name, price, grams, description }) => {
+const MenuItem = ({ name, name__japanease, price, grams, description }) => {
   return (
     <div className="hoshizora__menu-list_item">
       <div className="hoshizora__menu-list-name">
+        <p className="hoshizora__japanease-text p__kaisei-tokumin">{name__japanease}</p>
         <p className="p__kaisei-tokumin">{name}</p>
         <div className="hoshizora__menu-list-description">
           <p className="p__kaisei-tokumin">
@@ -35,8 +36,15 @@ const MenuItem = ({ name, price, grams, description }) => {
 const MenuList = ({ category }) => {
   return (
     <div className="hoshizora__menu-list">
-      {category.map(({ id, name, price, grams, description }) => (
-        <MenuItem key={id} name={name} price={price} grams={grams} description={description} />
+      {category.map(({ id, name, name__japanease, price, grams, description }) => (
+        <MenuItem
+          key={id}
+          name={name}
+          name__japanease={name__japanease}
+          price={price}
+          grams={grams}
+          description={description}
+        />
       ))}
     </div>
   );
--- src/container/Menu/Menu.jsx
+++ src/container/Menu/Menu.jsx
@@ -13,21 +13,25 @@ const Menu = () => {
       <div className="hoshizora__menu-bento">
         <div className="hoshizora__menu-bento_col">
           <div className="hoshizora__menu-bento_box bg_sky-gradient">
+            <h3 className="hoshizora__japanease-text p__kaisei-tokumin">A5ランクの牛肉</h3>
             <h3 className="p__yusei-magic">Beef (A5)</h3>
             <MenuList category={beefs} />
           </div>
           <div className="hoshizora__menu-bento_box bg_sky-gradient">
+            <h3 className="hoshizora__japanease-text p__kaisei-tokumin">ラーメン</h3>
             <h3 className="p__yusei-magic">Ramen</h3>
             <MenuList category={ramen} />
           </div>
         </div>
         <div className="hoshizora__menu-bento_col">
           <div className="hoshizora__menu-bento_box bg_sky-gradient">
+            <h3 className="hoshizora__japanease-text p__kaisei-tokumin">刺身</h3>
             <h3 className="p__yusei-magic">Sashimi</h3>
             <MenuList category={sashimi} />
           </div>
           <div className="hoshizora__menu-bento_box bg_sky-gradient">
-            <h3 className="p__yusei-magic">Sea-food</h3>
+            <h3 className="hoshizora__japanease-text p__kaisei-tokumin">Beef (A5)</h3>
+            <h3 className="p__yusei-magic">シーフード</h3>
             <MenuList category={sea__food} />
           </div>
         </div>
