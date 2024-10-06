a9f8e101035691fafd14ca4fde4471dc613322f0
Damian Szkudlarek <damian.szkudlarek@yuush.com>
2024-10-06 03:00:47 +0200

    Intro section done for desktop devices, needs optymalization for the rest.

--- src/container/Intro/Intro.jsx
+++ src/container/Intro/Intro.jsx
@@ -1,13 +1,34 @@
-import React from 'react';
-
+import React, { useRef, useState } from 'react';
+import food from '../../assets/food_video.mp4';
 import './Intro.css';
+import { BsFillPlayFill, BsPauseFill } from 'react-icons/bs';
 
 const Intro = () => {
+  const [playVideo, setPlayVideo] = useState(false);
+  const vidRef = useRef();
+
+  const handleVideo = () => {
+    setPlayVideo((prevPlayVideo) => !prevPlayVideo);
+
+    if (playVideo) {
+      vidRef.current.pause();
+    } else {
+      vidRef.current.play();
+    }
+  };
   return (
-    <div>
-      Intro
+    <div className="hoshizora__video">
+      <video src={food} ref={vidRef} type="video/mp4" loop controls={false} muted />
+      <div
+        className="hoshizora__video-overlay flex_center"
+        style={playVideo ? { background: 'rgba(0, 0, 0, 0.3)' } : { background: 'rgba(0, 0, 0, 0.65)' }}
+      >
+        <div className="hoshizora__video-overlay_circle flex_center" onClick={handleVideo}>
+          {playVideo ? <BsPauseFill color="#ecc96b" fontSize={30} /> : <BsFillPlayFill color="#ecc96b" fontSize={30} />}
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Intro;
