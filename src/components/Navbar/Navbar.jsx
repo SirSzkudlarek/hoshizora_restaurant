47ba6febdb8d0e6f8a3a526142120b9804c11594
Damian Szkudlarek <damian.szkudlarek@yuush.com>
2024-10-23 08:20:01 +0200

    Adding animation to header

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { useState } from 'react';
 import images from '../../constants/images';
 import './Navbar.css';
@@ -6,38 +6,63 @@ import { GiHamburgerMenu } from 'react-icons/gi';
 import { GiChopsticks } from 'react-icons/gi';
 import StarsBackground from '../StarsBackground/StarsBackground';
 
+import gsap from 'gsap';
+import { useGSAP } from '@gsap/react';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
+gsap.registerPlugin(ScrollTrigger);
+
+const animsScrollTrigger = {
+  scrollTrigger: {
+    trigger: '.hoshizora__navbar',
+    start: 'top 75%', // when the top of the trigger hits the top of the viewport
+  },
+};
+
 const Navbar = () => {
   const [toggleMenu, setToggleMenu] = useState(false);
 
+  const navbar_containerRef = useRef();
+
+  useGSAP(() => {
+    gsap.from(navbar_containerRef.current.querySelectorAll('.navbar_nav_item'), {
+      y: 30,
+      opacity: 0,
+      duration: 1.25,
+      stagger: 0.3,
+      ...animsScrollTrigger,
+    });
+  });
+
   return (
-    <nav className="hoshizora__navbar" id="home">
-      <div className="hoshizora__navbar-logo">
+    <nav ref={navbar_containerRef} className="hoshizora__navbar" id="home">
+      <div className="hoshizora__navbar-logo navbar_nav_item">
         <img src={images.hoshizora} alt="hoshizora logo" />
       </div>
       <ul className="hoshizora__navbar-links">
-        <li className="p__kaisei-tokumin">
+        <li className="p__kaisei-tokumin navbar_nav_item">
           <a href="#home">Home</a>
         </li>
-        <li className="p__kaisei-tokumin">
+        <li className="p__kaisei-tokumin navbar_nav_item">
           <a href="#about">About</a>
         </li>
-        <li className="p__kaisei-tokumin">
+        <li className="p__kaisei-tokumin navbar_nav_item">
           <a href="#menu">Menu</a>
         </li>
-        <li className="p__kaisei-tokumin">
+        <li className="p__kaisei-tokumin navbar_nav_item">
           <a href="#awards">Awards</a>
         </li>
-        <li className="p__kaisei-tokumin">
+        <li className="p__kaisei-tokumin navbar_nav_item">
           <a href="#contact">Contact</a>
         </li>
       </ul>
       <div className="hoshizora__navbar-login">
         <a href="#login" className="p__kaisei-tokumin">
-          Log In / Registration
+          <span className="navbar_nav_item">Log In / Registration</span>
         </a>
-        <div />
+        <div className="navbar_nav_item" />
         <a href="/" className="p__kaisei-tokumin">
-          Book Table
+          <span className="navbar_nav_item">Book Table</span>
         </a>
       </div>
       <div className="hoshizora__navbar-smallscreen">
--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -9,21 +9,56 @@ import { MouseParallax } from 'react-just-parallax';
 
 import './Header.css';
 
+import gsap from 'gsap';
+import { useGSAP } from '@gsap/react';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
+gsap.registerPlugin(ScrollTrigger);
+
+const animsScrollTrigger = {
+  scrollTrigger: {
+    trigger: '.hoshizora__header',
+    start: 'top 75%', // when the top of the trigger hits the top of the viewport
+  },
+};
+
 const Header = () => {
   const parallaxRef = useRef(null);
+  const header_contentRef = useRef();
+
+  useGSAP(() => {
+    gsap.from(header_contentRef.current.querySelectorAll('.header_content_anim'), {
+      y: 30,
+      opacity: 0,
+      duration: 1.5,
+      stagger: 0.75,
+      ...animsScrollTrigger,
+    });
+    gsap.from('.hoshizora__header-img', {
+      y: 100,
+      opacity: 0,
+      delay: 1,
+      duration: 1.5,
+      ...animsScrollTrigger,
+    });
+  });
 
   return (
     <header className="hoshizora__header bg_sky-gradient hoshizora__wrapper section__padding" ref={parallaxRef}>
-      <div className="hoshizora__header-introduction">
-        <SubHeading title="Dare to taste heavenly food" />
-        <h1 className="hoshizora__header-h1">Traditional Japanese Dining</h1>
-        <p className="p__kaisei-tokumin" style={{ margin: '2rem 0' }}>
+      <div ref={header_contentRef} className="hoshizora__header-introduction">
+        <div className="header_content_anim">
+          <SubHeading title="Dare to taste heavenly food" />
+        </div>
+        <h1 className="hoshizora__header-h1 header_content_anim">Traditional Japanese Dining</h1>
+        <p className="p__kaisei-tokumin header_content_anim" style={{ margin: '2rem 0' }}>
           Sit tellus lobortis sed senectus vivamus molestie. Condimentum volutpat morbi facilisis quam scelerisque
           sapien. Et, penatibus aliquam amet tellus{' '}
         </p>
-        <button type="button" className="custom__button">
-          Explore Menu
-        </button>
+        <div className="header_content_anim">
+          <button type="button" className="custom__button">
+            Explore Menu
+          </button>
+        </div>
       </div>
       <div className="hoshizora__header-img">
         <img src={images.headerIntroduction} alt="header-img" />
